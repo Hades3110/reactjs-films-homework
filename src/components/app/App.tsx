ad93f7a217fc71b1759954ac762a8856f7f09445
Hrant Grishyan <grishyan.hrant@gmail.com>
2021-11-24 18:15:01 +0400

    add bg image

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -3,9 +3,11 @@ import './app.scss';
 import Header from '../header/Header';
 import Main from '../main/Main';
 import Footer from '../footer/Footer';
+import Search from '../search/Search';
 
 const App = () => {
         return <>
+                <Search />
                 <Header />
                 <Main />
                 <Footer />
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,45 @@
-import React from 'react'
-import Search from './search/Search'
+import React, { useState, useEffect } from 'react'
+import FilmService from '../../services/filmService'
+import { GenresInterface } from './types'
 import './header.scss'
-import Title from './title/Title'
 
-const Header = () => {
+const Header: React.FC = () => {
+    const filmService = new FilmService();
+    const [filmInfo, setFilmInfo] = useState<any>([]);
+    useEffect(() => {
+        filmService.getHeadFilm()
+            .then(res => setFilmInfo(res))
+    }, [])
+    console.log(filmInfo)
+    const bgImage: string = filmInfo.backdrop_path;
+    console.log(bgImage)
     return (
-        <header>
-            <Search />
-            <Title />
+        <header style={{ 
+            backgroundImage: `url("https://image.tmdb.org/t/p/original${bgImage}")`}}>
+            <div className="infoAndRating">
+            <h1>{filmInfo.title}</h1>
+            <div className="infoAndRating__moreInfo">
+                <ul>
+                    {filmInfo.genres ? filmInfo.genres.map((el: GenresInterface) => <li key={el.id}>{el.name}</li>) : ""}
+                    <li>|</li>
+                    <li>1h 46m</li>
+                </ul>
+            </div>
+            <div className="infoAndRating__ratingAndBtn">
+                <div className="infoAndRating__ratingAndBtn__rating">
+                    <span className="infoAndRating__ratingAndBtn__rating__stars">
+                        &#9733; &#9733; &#9733; &#9733; &#9733;
+                    </span>
+                    <div className="infoAndRating__ratingAndBtn__rating__num">{filmInfo.vote_average/2}</div>
+                </div>
+                <div className="infoAndRating__ratingAndBtn__btns">
+                    <button className="infoAndRating__ratingAndBtn__btns__watch">Watch now</button>
+                    <button className="infoAndRating__ratingAndBtn__btns__view">View Info</button>
+                </div>
+            </div>
+        </div>
         </header>
     )
 }
 
-export default Header
+export default Header;
