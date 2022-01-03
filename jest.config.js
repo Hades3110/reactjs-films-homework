7081851a2a7b893865d3f894d920c7453ce33577
Hrant <grishyan.hrant@gmail.com>
2022-01-03 17:49:22 +0400

    fix jest

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
     verbose: true,
     preset: 'ts-jest',
     setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
-    testEnvironment: "jsdom"
+    testEnvironment: "jsdom",
+    moduleNameMapper: {
+        '\\.(scss)$': 'identity-obj-proxy',
+    }
 };
--- src/components/footer/__tests__/Footer.test.tsx
+++ src/components/footer/__tests__/Footer.test.tsx
@@ -1,10 +1,10 @@
-import renderer from "react-test-renderer";
+import { create } from "react-test-renderer";
 import Footer from "../Footer";
 
 describe('Footer Component', () => {
 
     it('Footer snapshot test', () => {
-        const component = renderer.create(<Footer />);
+        const component = create(<Footer />);
         const tree = component.toJSON();
         expect(tree).toMatchSnapshot();
     });
